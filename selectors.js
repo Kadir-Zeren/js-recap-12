11535ee834a2f289294309b0703a9aa73923ea29
kadir-zeren <k4dzeren@gmail.com>
2023-11-27 16:16:08 +0300

    v1

--- selectors.js
+++ selectors.js
@@ -1,1 +1,6 @@
-console.log("first");
+document.title = "DOM DOM";
+
+const myHeader = document.getElementById("header");
+console.log(myHeader);
+myHeader.style.backgroundColor = "red";
+myHeader.style.color = "white";
